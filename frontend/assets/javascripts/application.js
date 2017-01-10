372533b31ada29bc3dc02596fbb5c1e57c35e2ad
meriy100 <ttattataa@gmail.com>
2017-01-10 15:48:06 +0900

    redux logger

--- frontend/assets/javascripts/application.js
+++ frontend/assets/javascripts/application.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
-import { createStore, combineReducers } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import { Router, Route, Link, browserHistory } from 'react-router'
 import { syncHistoryWithStore, routerReducer } from 'react-router-redux'
+import createLogger from 'redux-logger';
 import posts from './reducers/posts'
 import Ps from './containers/Ps'
 
-let store = createStore(posts)
+let store = createStore(posts, applyMiddleware(createLogger()))
 
 render(
   <Provider store={store}>
