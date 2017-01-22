f32f0168d4b1edba6f0ed00acb90b5f4bbf820b2
meriy100 <ttattataa@gmail.com>
2017-01-22 09:57:28 +0900

    shared add node_module

--- config/deploy.rb
+++ config/deploy.rb
@@ -29,7 +29,7 @@ set :linked_files, fetch(:linked_files, []).push('config/database.yml', ".env")
 
 # Default value for linked_dirs is []
 # set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system')
-set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'node_modules')
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
