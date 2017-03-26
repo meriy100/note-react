d20f036df5beaabd5459c6f77edebec07cdad4e8
meriy100 <ttattataa@gmail.com>
2017-03-26 17:38:16 +0900

    css fix

--- db/fixtures/development/post.rb
+++ db/fixtures/development/post.rb
@@ -2,7 +2,8 @@ post_attirbutes = (1..10).map do |idx|
   {
     id: idx,
     path: idx % 2 == 0 ? "template/"+"#{idx}" : "hoge/"+"#{idx}",
-    body: "%{me}ppp%{year}"+"#{Faker::Lorem.sentence(5)}"
+    body: "%{me}ppp%{year}"+"#{Faker::Lorem.sentence(5)}",
+    created_user: User.all.sample
   }
 end
 Post.seed(:id, post_attirbutes)
