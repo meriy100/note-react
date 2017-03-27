b3379099fe37673b06b102dfd10a0789f2eed0af
yaaaaashiki <yaaaaakishi@gmail.com>
2017-03-27 17:08:10 +0900

    add host column

--- db/schema.rb
+++ db/schema.rb
@@ -56,6 +56,7 @@ ActiveRecord::Schema.define(version: 20170326135855) do
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name"
     t.string   "email",                            null: false
+    t.boolean  "host",             default: false, null: false
     t.datetime "created_at",                       null: false
     t.datetime "updated_at",                       null: false
     t.string   "crypted_password"
